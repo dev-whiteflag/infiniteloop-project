a6437256721fc6542853646eaf20f4f6e17144fa
Whiteflag <dev.whiteflag@gmail.com>
2017-12-07 16:54:01 -0200

    Adicionado UE4Project
    Projeto base com suporte a C++.

---
+++ unrealengineproject/Intermediate/Build/Win64/UE4Editor/Inc/unrealengineproject/unrealengineprojectCharacter.generated.h
@@ -0,0 +1,94 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef UNREALENGINEPROJECT_unrealengineprojectCharacter_generated_h
+#error "unrealengineprojectCharacter.generated.h already included, missing '#pragma once' in unrealengineprojectCharacter.h"
+#endif
+#define UNREALENGINEPROJECT_unrealengineprojectCharacter_generated_h
+
+#define unrealengineproject_Source_unrealengineproject_unrealengineprojectCharacter_h_12_RPC_WRAPPERS
+#define unrealengineproject_Source_unrealengineproject_unrealengineprojectCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define unrealengineproject_Source_unrealengineproject_unrealengineprojectCharacter_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAunrealengineprojectCharacter(); \
+	friend UNREALENGINEPROJECT_API class UClass* Z_Construct_UClass_AunrealengineprojectCharacter(); \
+public: \
+	DECLARE_CLASS(AunrealengineprojectCharacter, ACharacter, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/unrealengineproject"), NO_API) \
+	DECLARE_SERIALIZER(AunrealengineprojectCharacter) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define unrealengineproject_Source_unrealengineproject_unrealengineprojectCharacter_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAunrealengineprojectCharacter(); \
+	friend UNREALENGINEPROJECT_API class UClass* Z_Construct_UClass_AunrealengineprojectCharacter(); \
+public: \
+	DECLARE_CLASS(AunrealengineprojectCharacter, ACharacter, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/unrealengineproject"), NO_API) \
+	DECLARE_SERIALIZER(AunrealengineprojectCharacter) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define unrealengineproject_Source_unrealengineproject_unrealengineprojectCharacter_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AunrealengineprojectCharacter(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AunrealengineprojectCharacter) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AunrealengineprojectCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AunrealengineprojectCharacter); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AunrealengineprojectCharacter(AunrealengineprojectCharacter&&); \
+	NO_API AunrealengineprojectCharacter(const AunrealengineprojectCharacter&); \
+public:
+
+
+#define unrealengineproject_Source_unrealengineproject_unrealengineprojectCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AunrealengineprojectCharacter(AunrealengineprojectCharacter&&); \
+	NO_API AunrealengineprojectCharacter(const AunrealengineprojectCharacter&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AunrealengineprojectCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AunrealengineprojectCharacter); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AunrealengineprojectCharacter)
+
+
+#define unrealengineproject_Source_unrealengineproject_unrealengineprojectCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	FORCEINLINE static uint32 __PPO__CameraBoom() { return STRUCT_OFFSET(AunrealengineprojectCharacter, CameraBoom); } \
+	FORCEINLINE static uint32 __PPO__FollowCamera() { return STRUCT_OFFSET(AunrealengineprojectCharacter, FollowCamera); }
+
+
+#define unrealengineproject_Source_unrealengineproject_unrealengineprojectCharacter_h_9_PROLOG
+#define unrealengineproject_Source_unrealengineproject_unrealengineprojectCharacter_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	unrealengineproject_Source_unrealengineproject_unrealengineprojectCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	unrealengineproject_Source_unrealengineproject_unrealengineprojectCharacter_h_12_RPC_WRAPPERS \
+	unrealengineproject_Source_unrealengineproject_unrealengineprojectCharacter_h_12_INCLASS \
+	unrealengineproject_Source_unrealengineproject_unrealengineprojectCharacter_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define unrealengineproject_Source_unrealengineproject_unrealengineprojectCharacter_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	unrealengineproject_Source_unrealengineproject_unrealengineprojectCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	unrealengineproject_Source_unrealengineproject_unrealengineprojectCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	unrealengineproject_Source_unrealengineproject_unrealengineprojectCharacter_h_12_INCLASS_NO_PURE_DECLS \
+	unrealengineproject_Source_unrealengineproject_unrealengineprojectCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID unrealengineproject_Source_unrealengineproject_unrealengineprojectCharacter_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS

bda56120f8a0079b27c3cd9f996be70ab945a5d7
icarusMQ <icaromarx29@gmail.com>
2017-12-08 16:58:44 -0200

    rebuild
    recomendado pela unreal

---
+++ unrealengineproject/Intermediate/Build/Mac/UE4Editor/Inc/unrealengineproject/unrealengineprojectGameMode.gen.cpp
@@ -0,0 +1,69 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "GeneratedCppIncludes.h"
+#include "unrealengineprojectGameMode.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeunrealengineprojectGameMode() {}
+// Cross Module References
+	UNREALENGINEPROJECT_API UClass* Z_Construct_UClass_AunrealengineprojectGameMode_NoRegister();
+	UNREALENGINEPROJECT_API UClass* Z_Construct_UClass_AunrealengineprojectGameMode();
+	ENGINE_API UClass* Z_Construct_UClass_AGameModeBase();
+	UPackage* Z_Construct_UPackage__Script_unrealengineproject();
+// End Cross Module References
+	void AunrealengineprojectGameMode::StaticRegisterNativesAunrealengineprojectGameMode()
+	{
+	}
+	UClass* Z_Construct_UClass_AunrealengineprojectGameMode_NoRegister()
+	{
+		return AunrealengineprojectGameMode::StaticClass();
+	}
+	UClass* Z_Construct_UClass_AunrealengineprojectGameMode()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			static UObject* (*const DependentSingletons[])() = {
+				(UObject* (*)())Z_Construct_UClass_AGameModeBase,
+				(UObject* (*)())Z_Construct_UPackage__Script_unrealengineproject,
+			};
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[] = {
+				{ "HideCategories", "Info Rendering MovementReplication Replication Actor Input Movement Collision Rendering Utilities|Transformation" },
+				{ "IncludePath", "unrealengineprojectGameMode.h" },
+				{ "ModuleRelativePath", "unrealengineprojectGameMode.h" },
+				{ "ShowCategories", "Input|MouseInput Input|TouchInput" },
+			};
+#endif
+			static const FCppClassTypeInfoStatic StaticCppClassTypeInfo = {
+				TCppClassTypeTraits<AunrealengineprojectGameMode>::IsAbstract,
+			};
+			static const UE4CodeGen_Private::FClassParams ClassParams = {
+				&AunrealengineprojectGameMode::StaticClass,
+				DependentSingletons, ARRAY_COUNT(DependentSingletons),
+				0x00880288u,
+				nullptr, 0,
+				nullptr, 0,
+				nullptr,
+				&StaticCppClassTypeInfo,
+				nullptr, 0,
+				METADATA_PARAMS(Class_MetaDataParams, ARRAY_COUNT(Class_MetaDataParams))
+			};
+			UE4CodeGen_Private::ConstructUClass(OuterClass, ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(AunrealengineprojectGameMode, 4029007321);
+	static FCompiledInDefer Z_CompiledInDefer_UClass_AunrealengineprojectGameMode(Z_Construct_UClass_AunrealengineprojectGameMode, &AunrealengineprojectGameMode::StaticClass, TEXT("/Script/unrealengineproject"), TEXT("AunrealengineprojectGameMode"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(AunrealengineprojectGameMode);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
---
+++ unrealengineproject/Intermediate/Build/Mac/UE4Editor/Inc/unrealengineproject/unrealengineprojectGameMode.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef UNREALENGINEPROJECT_unrealengineprojectGameMode_generated_h
+#error "unrealengineprojectGameMode.generated.h already included, missing '#pragma once' in unrealengineprojectGameMode.h"
+#endif
+#define UNREALENGINEPROJECT_unrealengineprojectGameMode_generated_h
+
+#define unrealengineproject_Source_unrealengineproject_unrealengineprojectGameMode_h_12_RPC_WRAPPERS
+#define unrealengineproject_Source_unrealengineproject_unrealengineprojectGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define unrealengineproject_Source_unrealengineproject_unrealengineprojectGameMode_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAunrealengineprojectGameMode(); \
+	friend UNREALENGINEPROJECT_API class UClass* Z_Construct_UClass_AunrealengineprojectGameMode(); \
+public: \
+	DECLARE_CLASS(AunrealengineprojectGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/unrealengineproject"), UNREALENGINEPROJECT_API) \
+	DECLARE_SERIALIZER(AunrealengineprojectGameMode) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define unrealengineproject_Source_unrealengineproject_unrealengineprojectGameMode_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAunrealengineprojectGameMode(); \
+	friend UNREALENGINEPROJECT_API class UClass* Z_Construct_UClass_AunrealengineprojectGameMode(); \
+public: \
+	DECLARE_CLASS(AunrealengineprojectGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/unrealengineproject"), UNREALENGINEPROJECT_API) \
+	DECLARE_SERIALIZER(AunrealengineprojectGameMode) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define unrealengineproject_Source_unrealengineproject_unrealengineprojectGameMode_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	UNREALENGINEPROJECT_API AunrealengineprojectGameMode(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AunrealengineprojectGameMode) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(UNREALENGINEPROJECT_API, AunrealengineprojectGameMode); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AunrealengineprojectGameMode); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	UNREALENGINEPROJECT_API AunrealengineprojectGameMode(AunrealengineprojectGameMode&&); \
+	UNREALENGINEPROJECT_API AunrealengineprojectGameMode(const AunrealengineprojectGameMode&); \
+public:
+
+
+#define unrealengineproject_Source_unrealengineproject_unrealengineprojectGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	UNREALENGINEPROJECT_API AunrealengineprojectGameMode(AunrealengineprojectGameMode&&); \
+	UNREALENGINEPROJECT_API AunrealengineprojectGameMode(const AunrealengineprojectGameMode&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(UNREALENGINEPROJECT_API, AunrealengineprojectGameMode); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AunrealengineprojectGameMode); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AunrealengineprojectGameMode)
+
+
+#define unrealengineproject_Source_unrealengineproject_unrealengineprojectGameMode_h_12_PRIVATE_PROPERTY_OFFSET
+#define unrealengineproject_Source_unrealengineproject_unrealengineprojectGameMode_h_9_PROLOG
+#define unrealengineproject_Source_unrealengineproject_unrealengineprojectGameMode_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	unrealengineproject_Source_unrealengineproject_unrealengineprojectGameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	unrealengineproject_Source_unrealengineproject_unrealengineprojectGameMode_h_12_RPC_WRAPPERS \
+	unrealengineproject_Source_unrealengineproject_unrealengineprojectGameMode_h_12_INCLASS \
+	unrealengineproject_Source_unrealengineproject_unrealengineprojectGameMode_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define unrealengineproject_Source_unrealengineproject_unrealengineprojectGameMode_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	unrealengineproject_Source_unrealengineproject_unrealengineprojectGameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	unrealengineproject_Source_unrealengineproject_unrealengineprojectGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	unrealengineproject_Source_unrealengineproject_unrealengineprojectGameMode_h_12_INCLASS_NO_PURE_DECLS \
+	unrealengineproject_Source_unrealengineproject_unrealengineprojectGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID unrealengineproject_Source_unrealengineproject_unrealengineprojectGameMode_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS

9e606f6d3d3aa5345332060c5fc732147f3597c5
icarus <icaromarx29@gmail.com>
2018-01-17 18:46:37 -0200

    rebuild
    feito pra entrado em um novo computador

---
+++ unrealengineproject/Intermediate/Build/Mac/UE4Editor/Inc/unrealengineproject/unrealengineproject.generated.cpp
@@ -0,0 +1,150 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "GeneratedCppIncludes.h"
+#include "unrealengineproject.generated.dep.h"
+PRAGMA_DISABLE_OPTIMIZATION
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCode1unrealengineproject() {}
+#if USE_COMPILED_IN_NATIVES
+// Cross Module References
+	ENGINE_API class UClass* Z_Construct_UClass_ACharacter();
+	ENGINE_API class UClass* Z_Construct_UClass_UCameraComponent_NoRegister();
+	ENGINE_API class UClass* Z_Construct_UClass_USpringArmComponent_NoRegister();
+	ENGINE_API class UClass* Z_Construct_UClass_AGameModeBase();
+
+	UNREALENGINEPROJECT_API class UClass* Z_Construct_UClass_AunrealengineprojectCharacter_NoRegister();
+	UNREALENGINEPROJECT_API class UClass* Z_Construct_UClass_AunrealengineprojectCharacter();
+	UNREALENGINEPROJECT_API class UClass* Z_Construct_UClass_AunrealengineprojectGameMode_NoRegister();
+	UNREALENGINEPROJECT_API class UClass* Z_Construct_UClass_AunrealengineprojectGameMode();
+	UNREALENGINEPROJECT_API class UPackage* Z_Construct_UPackage__Script_unrealengineproject();
+	void AunrealengineprojectCharacter::StaticRegisterNativesAunrealengineprojectCharacter()
+	{
+	}
+	UClass* Z_Construct_UClass_AunrealengineprojectCharacter_NoRegister()
+	{
+		return AunrealengineprojectCharacter::StaticClass();
+	}
+	UClass* Z_Construct_UClass_AunrealengineprojectCharacter()
+	{
+		static UClass* OuterClass = NULL;
+		if (!OuterClass)
+		{
+			Z_Construct_UClass_ACharacter();
+			Z_Construct_UPackage__Script_unrealengineproject();
+			OuterClass = AunrealengineprojectCharacter::StaticClass();
+			if (!(OuterClass->ClassFlags & CLASS_Constructed))
+			{
+				UObjectForceRegistration(OuterClass);
+				OuterClass->ClassFlags |= 0x20800080;
+
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+				UProperty* NewProp_BaseLookUpRate = new(EC_InternalUseOnlyConstructor, OuterClass, TEXT("BaseLookUpRate"), RF_Public|RF_Transient|RF_MarkAsNative) UFloatProperty(CPP_PROPERTY_BASE(BaseLookUpRate, AunrealengineprojectCharacter), 0x0010000000020015);
+				UProperty* NewProp_BaseTurnRate = new(EC_InternalUseOnlyConstructor, OuterClass, TEXT("BaseTurnRate"), RF_Public|RF_Transient|RF_MarkAsNative) UFloatProperty(CPP_PROPERTY_BASE(BaseTurnRate, AunrealengineprojectCharacter), 0x0010000000020015);
+				UProperty* NewProp_FollowCamera = new(EC_InternalUseOnlyConstructor, OuterClass, TEXT("FollowCamera"), RF_Public|RF_Transient|RF_MarkAsNative) UObjectProperty(CPP_PROPERTY_BASE(FollowCamera, AunrealengineprojectCharacter), 0x00400000000a001d, Z_Construct_UClass_UCameraComponent_NoRegister());
+				UProperty* NewProp_CameraBoom = new(EC_InternalUseOnlyConstructor, OuterClass, TEXT("CameraBoom"), RF_Public|RF_Transient|RF_MarkAsNative) UObjectProperty(CPP_PROPERTY_BASE(CameraBoom, AunrealengineprojectCharacter), 0x00400000000a001d, Z_Construct_UClass_USpringArmComponent_NoRegister());
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+				OuterClass->ClassConfigName = FName(TEXT("Game"));
+				static TCppClassTypeInfo<TCppClassTypeTraits<AunrealengineprojectCharacter> > StaticCppClassTypeInfo;
+				OuterClass->SetCppTypeInfo(&StaticCppClassTypeInfo);
+				OuterClass->StaticLink();
+#if WITH_METADATA
+				UMetaData* MetaData = OuterClass->GetOutermost()->GetMetaData();
+				MetaData->SetValue(OuterClass, TEXT("HideCategories"), TEXT("Navigation"));
+				MetaData->SetValue(OuterClass, TEXT("IncludePath"), TEXT("unrealengineprojectCharacter.h"));
+				MetaData->SetValue(OuterClass, TEXT("ModuleRelativePath"), TEXT("unrealengineprojectCharacter.h"));
+				MetaData->SetValue(NewProp_BaseLookUpRate, TEXT("Category"), TEXT("Camera"));
+				MetaData->SetValue(NewProp_BaseLookUpRate, TEXT("ModuleRelativePath"), TEXT("unrealengineprojectCharacter.h"));
+				MetaData->SetValue(NewProp_BaseLookUpRate, TEXT("ToolTip"), TEXT("Base look up/down rate, in deg/sec. Other scaling may affect final rate."));
+				MetaData->SetValue(NewProp_BaseTurnRate, TEXT("Category"), TEXT("Camera"));
+				MetaData->SetValue(NewProp_BaseTurnRate, TEXT("ModuleRelativePath"), TEXT("unrealengineprojectCharacter.h"));
+				MetaData->SetValue(NewProp_BaseTurnRate, TEXT("ToolTip"), TEXT("Base turn rate, in deg/sec. Other scaling may affect final turn rate."));
+				MetaData->SetValue(NewProp_FollowCamera, TEXT("AllowPrivateAccess"), TEXT("true"));
+				MetaData->SetValue(NewProp_FollowCamera, TEXT("Category"), TEXT("Camera"));
+				MetaData->SetValue(NewProp_FollowCamera, TEXT("EditInline"), TEXT("true"));
+				MetaData->SetValue(NewProp_FollowCamera, TEXT("ModuleRelativePath"), TEXT("unrealengineprojectCharacter.h"));
+				MetaData->SetValue(NewProp_FollowCamera, TEXT("ToolTip"), TEXT("Follow camera"));
+				MetaData->SetValue(NewProp_CameraBoom, TEXT("AllowPrivateAccess"), TEXT("true"));
+				MetaData->SetValue(NewProp_CameraBoom, TEXT("Category"), TEXT("Camera"));
+				MetaData->SetValue(NewProp_CameraBoom, TEXT("EditInline"), TEXT("true"));
+				MetaData->SetValue(NewProp_CameraBoom, TEXT("ModuleRelativePath"), TEXT("unrealengineprojectCharacter.h"));
+				MetaData->SetValue(NewProp_CameraBoom, TEXT("ToolTip"), TEXT("Camera boom positioning the camera behind the character"));
+#endif
+			}
+		}
+		check(OuterClass->GetClass());
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(AunrealengineprojectCharacter, 128851360);
+	static FCompiledInDefer Z_CompiledInDefer_UClass_AunrealengineprojectCharacter(Z_Construct_UClass_AunrealengineprojectCharacter, &AunrealengineprojectCharacter::StaticClass, TEXT("/Script/unrealengineproject"), TEXT("AunrealengineprojectCharacter"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(AunrealengineprojectCharacter);
+	void AunrealengineprojectGameMode::StaticRegisterNativesAunrealengineprojectGameMode()
+	{
+	}
+	UClass* Z_Construct_UClass_AunrealengineprojectGameMode_NoRegister()
+	{
+		return AunrealengineprojectGameMode::StaticClass();
+	}
+	UClass* Z_Construct_UClass_AunrealengineprojectGameMode()
+	{
+		static UClass* OuterClass = NULL;
+		if (!OuterClass)
+		{
+			Z_Construct_UClass_AGameModeBase();
+			Z_Construct_UPackage__Script_unrealengineproject();
+			OuterClass = AunrealengineprojectGameMode::StaticClass();
+			if (!(OuterClass->ClassFlags & CLASS_Constructed))
+			{
+				UObjectForceRegistration(OuterClass);
+				OuterClass->ClassFlags |= 0x20880288;
+
+
+				static TCppClassTypeInfo<TCppClassTypeTraits<AunrealengineprojectGameMode> > StaticCppClassTypeInfo;
+				OuterClass->SetCppTypeInfo(&StaticCppClassTypeInfo);
+				OuterClass->StaticLink();
+#if WITH_METADATA
+				UMetaData* MetaData = OuterClass->GetOutermost()->GetMetaData();
+				MetaData->SetValue(OuterClass, TEXT("HideCategories"), TEXT("Info Rendering MovementReplication Replication Actor Input Movement Collision Rendering Utilities|Transformation"));
+				MetaData->SetValue(OuterClass, TEXT("IncludePath"), TEXT("unrealengineprojectGameMode.h"));
+				MetaData->SetValue(OuterClass, TEXT("ModuleRelativePath"), TEXT("unrealengineprojectGameMode.h"));
+				MetaData->SetValue(OuterClass, TEXT("ShowCategories"), TEXT("Input|MouseInput Input|TouchInput"));
+#endif
+			}
+		}
+		check(OuterClass->GetClass());
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(AunrealengineprojectGameMode, 2678831755);
+	static FCompiledInDefer Z_CompiledInDefer_UClass_AunrealengineprojectGameMode(Z_Construct_UClass_AunrealengineprojectGameMode, &AunrealengineprojectGameMode::StaticClass, TEXT("/Script/unrealengineproject"), TEXT("AunrealengineprojectGameMode"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(AunrealengineprojectGameMode);
+	UPackage* Z_Construct_UPackage__Script_unrealengineproject()
+	{
+		static UPackage* ReturnPackage = nullptr;
+		if (!ReturnPackage)
+		{
+			ReturnPackage = CastChecked<UPackage>(StaticFindObjectFast(UPackage::StaticClass(), nullptr, FName(TEXT("/Script/unrealengineproject")), false, false));
+			ReturnPackage->SetPackageFlags(PKG_CompiledIn | 0x00000000);
+			FGuid Guid;
+			Guid.A = 0x68214280;
+			Guid.B = 0x04794698;
+			Guid.C = 0x00000000;
+			Guid.D = 0x00000000;
+			ReturnPackage->SetGuid(Guid);
+
+		}
+		return ReturnPackage;
+	}
+#endif
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
+PRAGMA_ENABLE_OPTIMIZATION
---
+++ unrealengineproject/Intermediate/Build/Mac/UE4Editor/Inc/unrealengineproject/unrealengineproject.generated.dep.h
@@ -0,0 +1,13 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "GeneratedCppIncludes.h"
+#ifndef UNREALENGINEPROJECT_unrealengineprojectCharacter_generated_h
+	#include "unrealengineprojectCharacter.h"
+#endif
+#ifndef UNREALENGINEPROJECT_unrealengineprojectGameMode_generated_h
+	#include "unrealengineprojectGameMode.h"
+#endif
